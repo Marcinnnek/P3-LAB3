55af01a556d1c66c0820a907c53d0f0cffa105e3
Marcin Zuziak <porda1995@gmail.com>
2020-10-11 10:58:15 +0200

    OK.

---
+++ P3 LAB3 ZADANIE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P3_LAB3_ZADANIE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
