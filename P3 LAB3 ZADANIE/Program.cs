718b486ae92d152f6648731aed5ce785c6e78c0d
Marcin Zuziak <porda1995@gmail.com>
2020-10-19 18:11:46 +0200

    komentarze

--- P3 LAB3 ZADANIE/Program.cs
+++ P3 LAB3 ZADANIE/Program.cs
@@ -14,25 +14,25 @@ namespace P3_LAB3_ZADANIE
         {
             Console.Write("Podaj pierwszy wymiar tablicy: ");
             int firstDimension = int.Parse(Console.ReadLine());
+            int secondDimension;
 
-            int secondDimension;// = int.Parse(Console.ReadLine());
-
-            double[][] tablica = new double[firstDimension][];
+            double[][] tablica = new double[firstDimension][];  // definicja z deklaracją pierwszgo wymiaru tablicy poszarpanej
 
             Random rnd = new Random();
+
             for (int i = 0; i < tablica.Length; i++)
-            {
-                //tablica[i] = new double[secondDimension];
+            {    
                 Console.Write("Podaj drugi wymiar tablicy: ");
                 secondDimension = int.Parse(Console.ReadLine());
-                tablica[i] = new double[secondDimension];
-                //tablica[i] = new double[rnd.Next(10)];        
-                //tablica[i] = new double[2];                   #test
+                tablica[i] = new double[secondDimension];       // drugi wymiar podany przez użytkownika
+
+                //tablica[i] = new double[rnd.Next(10)];        // losowa długość drugiego wymiaru
+                //tablica[i] = new double[2];                   // test
                 for (int j = 0; j < tablica[i].Length; j++)
                 {
                     //Console.WriteLine(tablica[i].Length);
-                    tablica[i][j] = rnd.Next(100);
-                    //tablica[i][j] = 10;                       #test
+                    tablica[i][j] = rnd.Next(100);              // wartości z zakresu 0-100
+                    //tablica[i][j] = 10;                       // test
                 }
             }
 
@@ -42,8 +42,9 @@ namespace P3_LAB3_ZADANIE
             {
                 for (int j = 0; j < tablica[i].Length; j++)
                 {
-                    Console.WriteLine(suma);
                     suma += tablica[i][j];
+                    Console.WriteLine(suma);
+                    Console.WriteLine($"wartość po indeksem {i}, {j} {tablica[i][j]} ");
                 }
             }
             Console.WriteLine($"suma: {suma}");
