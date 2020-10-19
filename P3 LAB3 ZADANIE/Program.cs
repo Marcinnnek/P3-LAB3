c8547465bbee583c12ac9e13b52ee61d2b588049
Marcin Zuziak <porda1995@gmail.com>
2020-10-19 17:54:52 +0200

    suma tablicy poszarpanej

--- P3 LAB3 ZADANIE/Program.cs
+++ P3 LAB3 ZADANIE/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Dynamic;
 
 namespace P3_LAB3_ZADANIE
 {
@@ -6,7 +7,42 @@ namespace P3_LAB3_ZADANIE
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TableSum();
+        }
+
+        static void TableSum()
+        {
+            Console.Write("Podaj pierwszy wymiar tablicy: ");
+            int firstDimension = int.Parse(Console.ReadLine());
+            Console.Write("Podaj drugi wymiar tablicy: ");
+            int secondDimension = int.Parse(Console.ReadLine());
+
+            double[][] tablica = new double[firstDimension][];
+
+            Random rnd = new Random();
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                tablica[i] = new double[secondDimension];
+                //tablica[i] = new double[2];                   #test
+                for (int j = 0; j < tablica[i].Length; j++)
+                {
+                    //Console.WriteLine(tablica[i].Length);
+                    tablica[i][j] = rnd.Next(100);
+                    //tablica[i][j] = 10;                       #test
+                }
+            }
+
+            double suma=0;
+
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                for (int j = 0; j < tablica[i].Length; j++)
+                {
+                    Console.WriteLine(suma);
+                    suma += tablica[i][j];
+                }
+            }
+            Console.WriteLine($"suma: {suma}");
         }
     }
 }
